6dfb78fe36c2c97d7a50de0055c11d76e183cefc
Kushal Sharma <kushal.s@logicaldna.com>
2018-09-17 17:45:29 +0530

    First Commit

---
+++ Habitat-1.5/gulp-config.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+    var instanceRoot = "C:\\inetpub\\wwwroot\\NewInstance1";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "Habitat",
+    buildConfiguration: "Debug",
+    buildToolsVersion: 14.0,
+    buildMaxCpuCount: 0,
+    buildVerbosity: "minimal",
+    buildPlatform: "Any CPU",
+    publishPlatform: "AnyCpu",
+    runCleanBuilds: false
+  };
+  return config;
+}
